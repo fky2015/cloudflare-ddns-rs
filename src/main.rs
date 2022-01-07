4656cfd8f5b51028e998ad151771824132d81a5d
FKYnJYQ <loveress01@gmail.com>
2022-01-07 12:53:47 +0800

    feat: init project with minimal function.

---
+++ src/main.rs
@@ -0,0 +1,117 @@
+use reqwest;
+
+use dotenv::dotenv;
+use std::env;
+use anyhow::Error as AnyError;
+use pnet::datalink;
+use pnet::ipnetwork;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::net::IpAddr;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+enum ClientError {
+    #[error("data store disconnected")]
+    MissingDevice,
+    #[error("data store disconnected")]
+    RequestError,
+    #[error(transparent)]
+    Other(#[from] anyhow::Error),
+}
+
+fn get_default_interface() -> Option<datalink::NetworkInterface> {
+    datalink::interfaces()
+        .into_iter()
+        .find(|iface| !iface.is_loopback() && iface.is_up() && !iface.ips.is_empty())
+}
+
+fn get_ip_by_device_name(dev_name: String) -> Option<ipnetwork::IpNetwork> {
+    datalink::interfaces()
+        .into_iter()
+        .find(|iface| iface.name == dev_name)
+
+    .map(|iface| iface.ips.into_iter().find(|ip| ip.is_ipv4()))
+    .flatten()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), ClientError> {
+    dotenv().ok();
+
+    println!("Hello, world!");
+
+    let zone = env::var("ZONE").unwrap();
+    let token = env::var("TOKEN").unwrap();
+    let target_name = env::var("TARGET_NAME").unwrap();
+    let device_name = env::var("DEVICE").unwrap();
+
+    let target_ip = get_ip_by_device_name(device_name).ok_or(ClientError::MissingDevice)?.ip();
+    println!("{}", target_ip);
+
+    update_dns(zone, token, target_name, target_ip.to_string()).await?;
+
+    Ok(())
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct DNSRecords {
+    content: String,
+    id: String,
+    #[serde(rename = "type")]
+    _type: String,
+    name: String,
+    ttl: u32,
+}
+
+#[derive(Deserialize, Debug)]
+struct RetValue {
+    result: Vec<DNSRecords>,
+}
+
+async fn update_dns(zone: String, token: String, target_name: String, target_ip: String ) -> Result<(), AnyError> {
+
+    let client = reqwest::Client::new();
+    let body = client
+        .get(format!(
+            "{}/{}/{}",
+            "https://api.cloudflare.com/client/v4/zones", zone, "dns_records"
+        ))
+        .query(&[("name",&target_name)])
+        .bearer_auth(&token)
+        .send()
+        .await?
+        .json::<RetValue>()
+        .await?;
+
+    let mut record = body
+        .result
+        .into_iter()
+        .find(|record| record.name == target_name)
+        .unwrap();
+
+    if record.content == target_ip {
+        println!("no need to update");
+        return Ok(());
+    }
+
+    record.content = target_ip.into();
+    println!("body = {:#?}", record);
+
+    let ret = client
+        .patch(format!(
+            "{}/{}/{}/{}",
+            "https://api.cloudflare.com/client/v4/zones",
+            zone, "dns_records", record.id
+        ))
+        .json(&record)
+        .bearer_auth(token)
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+
+
+    println!("{:?}", ret);
+    Ok(())
+}
